efa132846f4d8d43db3ebb8076f65098fd675a99
Bogawick <150581294+Bogawick@users.noreply.github.com>
2023-11-14 20:40:09 -0600

    upt

--- app.js
+++ app.js
@@ -1,46 +1,79 @@
-const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot');
-const WebWhatsappProvider = require('@bot-whatsapp/provider/web-whatsapp');
-const MockAdapter = require('@bot-whatsapp/database/mock');
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
 
-// Flujo para solicitar el nombre del usuario
-const flowSolicitarNombre = addKeyword(['hola', 'ole', 'alo'])
-    .addAnswer(['Hola, bienvenido a mi servicio de programación web. ¿Cuál es tu nombre completo?']);
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
 
-// Flujo para mostrar la lista de servicios
-const flowListaServicios = addKeyword(['1', '2', '3', '4'])
-    .addAnswer([
-        'Elige el tipo de proyecto que te interesa:',
-        '1. Desarrollo de sitios web',
-        '2. Creación de aplicaciones web',
-        '3. Mantenimiento y actualización de sitios web',
-        '4. Otros servicios de programación',
-        'Escribe el número de tu elección.'
-    ])
-    .addAnswer(['¿Algo más en lo que pueda ayudarte?'])
-    .addAnswer(['Escribe "volver" para regresar al menú principal o "finalizar" para terminar la conversación.']);
+const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
 
-// Flujo para volver al menú principal
-const flowRegresarOMenu = addKeyword(['volver'])
-    .addAnswer(['Regresando al menú principal...'])
-    .addAnswer(flowSolicitarNombre.answers);  // Suponiendo que este es tu flujo principal
+const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
+    [
+        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
+        'https://bot-whatsapp.netlify.app/',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
 
-// Flujo para finalizar la conversación
-const flowFinalizar = addKeyword(['finalizar'])
-    .addAnswer(['Gracias por contactarme. ¡Que tengas un buen día!']);
+const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
+    [
+        '🙌 Aquí encontras un ejemplo rapido',
+        'https://bot-whatsapp.netlify.app/docs/example/',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
 
-/**
- * Esta es la función importante, la que realmente inicia el chatbot.
- */
+const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
+    [
+        '🚀 Puedes aportar tu granito de arena a este proyecto',
+        '[*opencollective*] https://opencollective.com/bot-whatsapp',
+        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
+        '[*patreon*] https://www.patreon.com/leifermendez',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowDiscord = addKeyword(['discord']).addAnswer(
+    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
+    .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
+    .addAnswer(
+        [
+            'te comparto los siguientes links de interes sobre el proyecto',
+            '👉 *doc* para ver la documentación',
+            '👉 *gracias*  para ver la lista de videos',
+            '👉 *discord* unirte al discord',
+        ],
+        null,
+        null,
+        [flowDocs, flowGracias, flowTuto, flowDiscord]
+    )
+
 const main = async () => {
-    const adapterDB = new MockAdapter();
-    const adapterFlow = createFlow([flowSolicitarNombre, flowListaServicios, flowRegresarOMenu, flowFinalizar]);
-    const adapterProvider = createProvider(WebWhatsappProvider);
-    
+    const adapterDB = new MockAdapter()
+    const adapterFlow = createFlow([flowPrincipal])
+    const adapterProvider = createProvider(BaileysProvider)
+
     createBot({
         flow: adapterFlow,
         provider: adapterProvider,
         database: adapterDB,
-    });
+    })
+
+    QRPortalWeb()
 }
 
-main();
+main()
